4ddc98ecf079f132a006fede7745f642ca12dbe0
unknown <rafael.massoni@gmail.com>
2022-05-18 20:32:53 -0300

    Implementando o Novo Projeto (Listas e Items) - PARTE 2

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,17 @@
 from django.test import TestCase
 from lists.models import Item
 
+class ListViewTest(TestCase):
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+
 class HomePageTest(TestCase):
 
     def test_uses_home_template(self):
